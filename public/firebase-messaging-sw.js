7977655a120b42c654d42432fdc1df0efd5a9353
Plesca Pavel <Plescapavel@yahoo.com>
2018-10-08 15:26:51 +0300

    Added senderId

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
 firebase.initializeApp({
-    messagingSenderId: 'your messagingSenderId' // troque pelo seu sender id 
+    messagingSenderId: '746451677638' // troque pelo seu sender id 
 });
 
 const messaging = firebase.messaging();
