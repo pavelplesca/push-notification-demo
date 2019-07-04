27a5062266a8475de80cdf1b57d67e792e13b883
Pavel Plesca <Pavel.Plesca@endava.com>
2019-07-04 17:58:44 +0300

    ...

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export class Constants {
   public static stsAuthority = 'https://liveid-dev.concardis.com/auth/realms/merchant';
   public static clientId = 'mycc-blue';
-  public static clientRoot = 'http://localhost:4200/';
+  public static clientRoot = 'https://70-532cert.azurewebsites.net/';
   public static clientScope = 'openid profile email';
 
   public static apiRoot = 'https://localhost/mobileapi/api/graphql';
